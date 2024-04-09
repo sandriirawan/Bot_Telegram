f7a81ad4097d4cec81d7706b0d03937446c9dfb6
sandri <sandri@bangsawancyberindo.co.id>
2024-04-09 20:30:56 +0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,139 @@
+const data = [
+  {
+    id: "1",
+    name: "Siteplan",
+    link: "",
+    category: 1,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712663997/siteplan1_zlyo8z.jpg",
+  },
+  {
+    id: "2",
+    name: "Siteplan",
+    link: "",
+    category: 1,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712664011/siteplan2_oz63m9.jpg",
+  },
+  {
+    id: "3",
+    name: "Siteplan",
+    link: "",
+    category: 1,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712664018/siteplan4_lzy4e8.jpg",
+  },
+  {
+    id: "4",
+    name: "Siteplan",
+    link: "",
+    category: 1,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712664032/siteplan3_hd6op7.jpg",
+  },
+  {
+    id: "4",
+    name: "pricelist",
+    link: "",
+    category: 2,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712664286/photo_6100445739512610001_x_jwihzq.jpg",
+  },
+  {
+    id: "5",
+    name: "gold",
+    link: "",
+    category: 3,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712664493/gold_2_1_e1osua.png",
+  },
+  {
+    id: "6",
+    name: "gold",
+    link: "",
+    category: 3,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712664473/gold_5_1_reux3w.png",
+  },
+  {
+    id: "7",
+    name: "gold",
+    link: "",
+    category: 3,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712664359/gold_interior_3_v0rmcp.png",
+  },
+  {
+    id: "8",
+    name: "gold",
+    link: "",
+    category: 3,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712664030/diamond_denah_2_j7ln3p.png",
+  },
+  {
+    id: "9",
+    name: "diamond",
+    link: "",
+    category: 4,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712664957/diamond_3_2_qjplap.png",
+  },
+  {
+    id: "10",
+    name: "diamond",
+    link: "",
+    category: 4,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712665051/diamond_4_1_1_n3rtl7.png",
+  },
+  {
+    id: "11",
+    name: "diamond",
+    link: "",
+    category: 4,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712664825/diamond_denah_2_qbhhph.png",
+  },
+  {
+    id: "12",
+    name: "platinum",
+    link: "",
+    category: 5,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712665272/photo_6100445739512610014_y_r61cot.jpg",
+  },
+  {
+    id: "13",
+    name: "platinum",
+    link: "",
+    category: 5,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712665432/platinum_interior_4_1_mslgfc.png",
+  },
+  {
+    id: "14",
+    name: "platinum",
+    link: "",
+    category: 5,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712665264/photo_6100445739512610013_y_xxdu6u.jpg",
+  },
+  {
+    id: "15",
+    name: "no rekening",
+    link: "",
+    category: 6,
+    image:
+      "https://res.cloudinary.com/ddn4pon2w/image/upload/v1712665573/photo_6100445739512610122_y_i2mynh.jpg",
+  },
+  {
+    id: "16",
+    name: "no rekening",
+    link: "https://forms.gle/65FKWZp4153wxCRBA",
+    category: 7,
+    image: "",
+  },
+];
+
+module.exports = data;
