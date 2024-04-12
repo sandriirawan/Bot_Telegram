87f99a4107d0286d8b6c97f221b57ab6fea3ac16
sandri <sandri@bangsawancyberindo.co.id>
2024-04-12 17:15:44 +0700

    fix: data

--- index.js
+++ index.js
@@ -1,193 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const TelegramBot = require("node-telegram-bot-api");
-const data = require("./src/config/data");
-const bodyParser = require("body-parser");
-
 dotenv.config();
 
+const bot = require("./src/module/telegramBot");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const token = process.env.TOKEN;
-const bot = new TelegramBot(token, { polling: true });
-
-app.use(bodyParser.json());
-
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  const messageText = msg.text;
-
-  switch (messageText) {
-    case "/start":
-      sendCommandsList(chatId);
-      break;
-    case "Go Back":
-      sendCommandsList(chatId);
-      break;
-    case "Siteplan":
-      sendPhotosByCategory(chatId, 1);
-      break;
-    case "Pricelist":
-      sendPhotosByCategory(chatId, 2);
-      break;
-    case "Type Villa":
-      sendVillaTypes(chatId);
-      break;
-    case "Gold":
-      sendPhotosByCategory(chatId, 3);
-      break;
-    case "Platinum":
-      sendPhotosByCategory(chatId, 5);
-      break;
-    case "Diamond":
-      sendPhotosByCategory(chatId, 4);
-      break;
-    case "Foto Lokasi SMC":
-      bot.sendPhoto(chatId, data.image, {
-        caption: "Here is the location photo!",
-      });
-      break;
-    case "Website Sagara":
-      bot.sendMessage(chatId, "Website of Sagara:");
-      break;
-    case "Copy Writing":
-      bot.sendMessage(chatId, "Copywriting Details:");
-      break;
-    case "No Rek SMC":
-      sendPhotosByCategory(chatId, 7);
-      break;
-    case "G Maps SMC":
-      sendGoogleMapsMessage(chatId);
-      break;
-    case "Gallery Marketing":
-      bot.sendMessage(chatId, "Marketing Gallery:");
-      break;
-    case "Form Survey Mitra":
-      const formSurveyMitra = data.filter((item) => item.category === 6);
-      bot.sendMessage(chatId, formSurveyMitra[0].link);
-      break;
-    case "Form Survey Agency":
-      bot.sendMessage(chatId, "Agency Survey Form:");
-      break;
-    case "SOP Marketing":
-      bot.sendMessage(chatId, "Marketing SOP:");
-      break;
-    case "View SMC":
-      bot.sendMessage(chatId, "SMC View:");
-      break;
-    case "Alur Follow Up":
-      bot.sendMessage(chatId, "Follow-up Flow:");
-      break;
-    case "Kontak Admin":
-      bot.sendMessage(chatId, "Contact Admin:");
-      break;
-    case "FAQ Sagara Mountain Ciwidey":
-      bot.sendMessage(chatId, "FAQ of Sagara Mountain Ciwidey:");
-      break;
-    default:
-      bot.sendMessage(chatId, "Unknown command. Type /start to begin.");
-      break;
-  }
-});
-
-function sendPhotosByCategory(chatId, category) {
-  const filteredData = data.filter((item) => item.category === category);
-  filteredData.forEach((item, index) => {
-    bot.sendPhoto(chatId, item.image);
-  });
-}
-
-function sendGoogleMapsMessage(chatId) {
-  const message = `
-    📍 Maps Lokasi Suvey
-    
-    Sagara Mountain Ciwidey Aset Eksklusif 
-    Masa Depan
-    
-    https://g.co/kgs/vYL92gn
-    _____
-    
-    Arahkan Rute Terbaik : 
-    Keluar Tol Soreang ke arah Ciwidey
-    
-    Pintu Tol Soreang  > 
-    Belok kiri di Masjid Raya Alfathu >
-    Lurus melewati 2 persimpangan >
-    Belok kiri di pertigaan Sadu > 
-    Alun-Alun Ciwidey > 
-    Belok kanan ke arah pasar Cibeureum 
-    / Sukawening > 
-    
-    Dari Tol Soreang kurang lebih 35 menit.
-  `;
-
-  bot.sendMessage(chatId, message);
-}
-
-function sendCommandsList(chatId) {
-  const keyboard = {
-    keyboard: [
-      [{ text: "Siteplan" }, { text: "Pricelist" }, { text: "Type Villa" }],
-      [
-        { text: "Foto Lokasi SMC" },
-        { text: "Website Sagara" },
-        { text: "Copy Writing" },
-      ],
-      [
-        { text: "No Rek SMC" },
-        { text: "G Maps SMC" },
-        { text: "Gallery Marketing" },
-      ],
-      [
-        { text: "Form Survey Mitra" },
-        { text: "Form Survey Agency" },
-        { text: "SOP Marketing" },
-      ],
-      [{ text: "View SMC" }, { text: "Alur Follow Up" }],
-      [{ text: "Kontak Admin" }],
-      [{ text: "FAQ Sagara Mountain Ciwidey" }],
-    ],
-    resize_keyboard: true,
-    one_time_keyboard: true,
-  };
-
-  bot.sendMessage(chatId, "Pilih Menu", {
-    reply_markup: JSON.stringify(keyboard),
-  });
-}
-
-function sendVillaTypes(chatId) {
-  const keyboard = {
-    keyboard: [
-      [{ text: "Gold" }, { text: "Platinum" }, { text: "Diamond" }],
-      [{ text: "Go Back" }],
-    ],
-    resize_keyboard: true,
-    one_time_keyboard: true,
-  };
-
-  bot.sendMessage(chatId, "Pilih Type", {
-    reply_markup: JSON.stringify(keyboard),
-  });
-}
-
-function setWebhook() {
-  const webhookUrl = `${process.env.WEBHOOK_URL}/bot${token}`;
-  bot
-    .setWebHook(webhookUrl)
-    .then(() => {
-      console.log(`Webhook berhasil diatur ke ${webhookUrl}`);
-    })
-    .catch((error) => {
-      console.error("Gagal mengatur webhook:", error);
-    });
-}
-
-app.post(`/bot${token}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
+app.use(express.json());
 
 app.get("/", (req, res) => {
   const data = {
@@ -197,7 +16,12 @@ app.get("/", (req, res) => {
   return res.json(data);
 });
 
+app.post(`/${process.env.TOKEN}`, (req, res) => {
+  console.log(req.body);
+  bot.processUpdate(req.body);
+  res.status(200).json({ message: "ok" });
+});
+
 app.listen(port, () => {
-  console.log(`Express server is listening on port ${port}`);
-  setWebhook();
+  console.log(`\n\nServer running on port ${port}.\n\n`);
 });
--- src/module/telegramBot.js
+++ src/module/telegramBot.js
@@ -1,200 +1,178 @@
-// const TelegramBot = require("node-telegram-bot-api");
-// const dotenv = require("dotenv");
-// const data = require("../config/data");
+const dotenv = require("dotenv");
+const TelegramBot = require("node-telegram-bot-api");
+const data = require("../config/data");
 
-// dotenv.config();
+dotenv.config();
 
-// const token = process.env.TOKEN;
-// const bot = new TelegramBot(token, { polling: true });
+const token = process.env.TOKEN;
+var bot;
 
-// bot.on("message", (msg) => {
-//   const chatId = msg.chat.id;
-//   const messageText = msg.text;
+if (process.env.NODE_ENV === "production") {
+  bot = new TelegramBot(token);
+  bot.setWebHook(process.env.WEBHOOK_URL + bot.token);
+  console.log("**** BOT initiated ***** ");
+} else {
+  bot = new TelegramBot(token, { polling: true });
+}
 
-//   switch (messageText) {
-//     case "/start":
-//       sendCommandsList(chatId);
-//       break;
-//     case "Go Back":
-//       sendCommandsList(chatId);
-//       break;
-//     case "Siteplan":
-//       const siteplan = data.filter((item) => item.category === 1);
-//       siteplan.forEach((item, index) => {
-//         setTimeout(() => {
-//           bot.sendPhoto(chatId, item.image);
-//         }, index * 1000);
-//       });
-//       break;
-//     case "Pricelist":
-//       const pricelist = data.filter((item) => item.category === 2);
-//       pricelist.forEach((item, index) => {
-//         setTimeout(() => {
-//           bot.sendPhoto(chatId, item.image);
-//         }, index * 1000);
-//       });
-//       break;
-//     case "Type Villa":
-//       sendVillaTypes(chatId);
-//       break;
-//     case "Gold":
-//       const gold = data.filter((item) => item.category === 3);
-//       gold.forEach((item, index) => {
-//         setTimeout(() => {
-//           bot.sendPhoto(chatId, item.image);
-//         }, index * 1000);
-//       });
-//       break;
-//     case "Platinum":
-//       const platinum = data.filter((item) => item.category === 5);
-//       platinum.forEach((item, index) => {
-//         setTimeout(() => {
-//           bot.sendPhoto(chatId, item.image);
-//         }, index * 1000);
-//       });
-//       break;
-//     case "Diamond":
-//       const diamond = data.filter((item) => item.category === 5);
-//       diamond.forEach((item, index) => {
-//         setTimeout(() => {
-//           bot.sendPhoto(chatId, item.image);
-//         }, index * 1000);
-//       });
-//       break;
-//     case "Foto Lokasi SMC":
-//       bot.sendPhoto(chatId, data.image, {
-//         caption: "Here is the location photo!",
-//       });
-//       break;
-//     case "Website Sagara":
-//       bot.sendMessage(chatId, "Website of Sagara:");
-//       bot.sendPhoto(chatId, data.image, { caption: "Website Screenshot" });
-//       break;
-//     case "Copy Writing":
-//       bot.sendMessage(chatId, "Copywriting Details:");
-//       bot.sendPhoto(chatId, data.image, { caption: "Copywriting Info" });
-//       break;
-//     case "No Rek SMC":
-//       const noRekData = data.find((item) => item.id === "15");
-//       if (noRekData) {
-//         bot.sendPhoto(chatId, noRekData.image, { caption: "No Rekening SMC" });
-//       } else {
-//         bot.sendMessage(chatId, "Error Server");
-//       }
-//       break;
-//     case "G Maps SMC":
-//       sendGoogleMapsMessage(chatId);
-//       break;
-//     case "Gallery Marketing":
-//       bot.sendMessage(chatId, "Marketing Gallery:");
-//       bot.sendPhoto(chatId, data.image, { caption: "Marketing Gallery" });
-//       break;
-//     case "Form Survey Mitra":
-//       const formSurveyMitra = data.filter((item) => item.category === 7);
-//       bot.sendMessage(chatId, formSurveyMitra[0].link);
-//       break;
-//     case "Form Survey Agency":
-//       bot.sendMessage(chatId, "Agency Survey Form:");
-//       bot.sendPhoto(chatId, data.image, { caption: "Survey Form" });
-//       break;
-//     case "SOP Marketing":
-//       bot.sendMessage(chatId, "Marketing SOP:");
-//       bot.sendPhoto(chatId, data.image, { caption: "SOP Info" });
-//       break;
-//     case "View SMC":
-//       bot.sendMessage(chatId, "SMC View:");
-//       bot.sendPhoto(chatId, data.image, { caption: "SMC View Info" });
-//       break;
-//     case "Alur Follow Up":
-//       bot.sendMessage(chatId, "Follow-up Flow:");
-//       bot.sendPhoto(chatId, data.image, { caption: "Follow-up Flow Info" });
-//       break;
-//     case "Kontak Admin":
-//       bot.sendMessage(chatId, "Contact Admin:");
-//       bot.sendPhoto(chatId, data.image, { caption: "Admin Contact Info" });
-//       break;
-//     case "FAQ Sagara Mountain Ciwidey":
-//       bot.sendMessage(chatId, "FAQ of Sagara Mountain Ciwidey:");
-//       bot.sendPhoto(chatId, data.image, { caption: "FAQ Info" });
-//       break;
-//     default:
-//       bot.sendMessage(chatId, "Unknown command. Type /start to begin.");
-//       break;
-//   }
-// });
+console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
 
-// function sendGoogleMapsMessage(chatId) {
-//   const message = `
-//     📍 Maps Lokasi Suvey
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  switch (messageText) {
+    case "/start":
+      sendCommandsList(chatId);
+      break;
+    case "Go Back":
+      sendCommandsList(chatId);
+      break;
+    case "Siteplan":
+      sendPhotosByCategory(chatId, 1);
+      break;
+    case "Pricelist":
+      sendPhotosByCategory(chatId, 2);
+      break;
+    case "Type Villa":
+      sendVillaTypes(chatId);
+      break;
+    case "Gold":
+      sendPhotosByCategory(chatId, 3);
+      break;
+    case "Platinum":
+      sendPhotosByCategory(chatId, 5);
+      break;
+    case "Diamond":
+      sendPhotosByCategory(chatId, 4);
+      break;
+    case "Foto Lokasi SMC":
+      bot.sendPhoto(chatId, data.image, {
+        caption: "Here is the location photo!",
+      });
+      break;
+    case "Website Sagara":
+      bot.sendMessage(chatId, "Website of Sagara:");
+      break;
+    case "Copy Writing":
+      bot.sendMessage(chatId, "Copywriting Details:");
+      break;
+    case "No Rek SMC":
+      sendPhotosByCategory(chatId, 7);
+      break;
+    case "G Maps SMC":
+      sendGoogleMapsMessage(chatId);
+      break;
+    case "Gallery Marketing":
+      bot.sendMessage(chatId, "Marketing Gallery:");
+      break;
+    case "Form Survey Mitra":
+      const formSurveyMitra = data.filter((item) => item.category === 6);
+      bot.sendMessage(chatId, formSurveyMitra[0].link);
+      break;
+    case "Form Survey Agency":
+      bot.sendMessage(chatId, "Agency Survey Form:");
+      break;
+    case "SOP Marketing":
+      bot.sendMessage(chatId, "Marketing SOP:");
+      break;
+    case "View SMC":
+      bot.sendMessage(chatId, "SMC View:");
+      break;
+    case "Alur Follow Up":
+      bot.sendMessage(chatId, "Follow-up Flow:");
+      break;
+    case "Kontak Admin":
+      bot.sendMessage(chatId, "Contact Admin:");
+      break;
+    case "FAQ Sagara Mountain Ciwidey":
+      bot.sendMessage(chatId, "FAQ of Sagara Mountain Ciwidey:");
+      break;
+    default:
+      bot.sendMessage(chatId, "Unknown command. Type /start to begin.");
+      break;
+  }
+});
+
+function sendPhotosByCategory(chatId, category) {
+  const filteredData = data.filter((item) => item.category === category);
+  filteredData.forEach((item, index) => {
+    bot.sendPhoto(chatId, item.image);
+  });
+}
+
+function sendGoogleMapsMessage(chatId) {
+  const message = `
+    📍 Maps Lokasi Suvey
     
-//     Sagara Mountain Ciwidey Aset Eksklusif 
-//     Masa Depan
+    Sagara Mountain Ciwidey Aset Eksklusif 
+    Masa Depan
     
-//     https://g.co/kgs/vYL92gn
-//     _____
+    https://g.co/kgs/vYL92gn
+    _____
     
-//     Arahkan Rute Terbaik : 
-//     Keluar Tol Soreang ke arah Ciwidey
+    Arahkan Rute Terbaik : 
+    Keluar Tol Soreang ke arah Ciwidey
     
-//     Pintu Tol Soreang  > 
-//     Belok kiri di Masjid Raya Alfathu >
-//     Lurus melewati 2 persimpangan >
-//     Belok kiri di pertigaan Sadu > 
-//     Alun-Alun Ciwidey > 
-//     Belok kanan ke arah pasar Cibeureum 
-//     / Sukawening > 
+    Pintu Tol Soreang  > 
+    Belok kiri di Masjid Raya Alfathu >
+    Lurus melewati 2 persimpangan >
+    Belok kiri di pertigaan Sadu > 
+    Alun-Alun Ciwidey > 
+    Belok kanan ke arah pasar Cibeureum 
+    / Sukawening > 
     
-//     Dari Tol Soreang kurang lebih 35 menit.
-//   `;
+    Dari Tol Soreang kurang lebih 35 menit.
+  `;
 
-//   bot.sendMessage(chatId, message);
-// }
+  bot.sendMessage(chatId, message);
+}
 
-// function sendCommandsList(chatId) {
-//   const keyboard = {
-//     keyboard: [
-//       [{ text: "Siteplan" }, { text: "Pricelist" }, { text: "Type Villa" }],
-//       [
-//         { text: "Foto Lokasi SMC" },
-//         { text: "Website Sagara" },
-//         { text: "Copy Writing" },
-//       ],
-//       [
-//         { text: "No Rek SMC" },
-//         { text: "G Maps SMC" },
-//         { text: "Gallery Marketing" },
-//       ],
-//       [
-//         { text: "Form Survey Mitra" },
-//         { text: "Form Survey Agency" },
-//         { text: "SOP Marketing" },
-//       ],
-//       [{ text: "View SMC" }, { text: "Alur Follow Up" }],
-//       [{ text: "Kontak Admin" }],
-//       [{ text: "FAQ Sagara Mountain Ciwidey" }],
-//     ],
-//     resize_keyboard: true,
-//     one_time_keyboard: true,
-//   };
+function sendCommandsList(chatId) {
+  const keyboard = {
+    keyboard: [
+      [{ text: "Siteplan" }, { text: "Pricelist" }, { text: "Type Villa" }],
+      [
+        { text: "Foto Lokasi SMC" },
+        { text: "Website Sagara" },
+        { text: "Copy Writing" },
+      ],
+      [
+        { text: "No Rek SMC" },
+        { text: "G Maps SMC" },
+        { text: "Gallery Marketing" },
+      ],
+      [
+        { text: "Form Survey Mitra" },
+        { text: "Form Survey Agency" },
+        { text: "SOP Marketing" },
+      ],
+      [{ text: "View SMC" }, { text: "Alur Follow Up" }],
+      [{ text: "Kontak Admin" }],
+      [{ text: "FAQ Sagara Mountain Ciwidey" }],
+    ],
+    resize_keyboard: true,
+    one_time_keyboard: true,
+  };
 
-//   bot.sendMessage(chatId, "Pilih Menu", {
-//     reply_markup: JSON.stringify(keyboard),
-//   });
-// }
+  bot.sendMessage(chatId, "Pilih Menu", {
+    reply_markup: JSON.stringify(keyboard),
+  });
+}
 
-// function sendVillaTypes(chatId) {
-//   const keyboard = {
-//     keyboard: [
-//       [{ text: "Gold" }, { text: "Platinum" }, { text: "Diamond" }],
-//       [{ text: "Go Back" }],
-//     ],
-//     resize_keyboard: true,
-//     one_time_keyboard: true,
-//   };
+function sendVillaTypes(chatId) {
+  const keyboard = {
+    keyboard: [
+      [{ text: "Gold" }, { text: "Platinum" }, { text: "Diamond" }],
+      [{ text: "Go Back" }],
+    ],
+    resize_keyboard: true,
+    one_time_keyboard: true,
+  };
 
-//   bot.sendMessage(chatId, "Pilih Type", {
-//     reply_markup: JSON.stringify(keyboard),
-//   });
-// }
+  bot.sendMessage(chatId, "Pilih Type", {
+    reply_markup: JSON.stringify(keyboard),
+  });
+}
 
-// module.exports = bot;
+module.exports = bot;
